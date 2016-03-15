94d4a23ecb26e0d71d8f81d69aaf2edf3dea98eb
RobertAKARobin <robertgfthomas@gmail.com>
2016-03-15 00:42:25 -0400

    Added flash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,22 +9,22 @@ class SessionsController < ApplicationController
     if User.exists?(username: input_username)
       @user = User.find_by(username: input_username)
       if @user.password === params[:user][:password]
-        puts "You're signed in!"
+        flash[:notice] = "You're signed in!"
         session[:user_id] = @user.id
         redirect_to root_path
       else
-        puts "Wrong password!"
+        flash[:alert] = "Wrong password!"
         redirect_to new_session_path
       end
     else
-      puts "That user doesn't exist!"
+      flash[:alert] = "That user doesn't exist!"
       redirect_to new_session_path
     end
   end
 
   def destroy
     reset_session
-    puts "You're signed out!"
+    flash[:notice] = "You're signed out!"
     redirect_to :root
   end
 
