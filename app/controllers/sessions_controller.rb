8bd67cff2033b1bfd4fb763510794cc12560653d
Tim Foley <thetimfoley@gmail.com>
2016-10-11 15:32:57 -0400

    setting up things

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,4 +13,27 @@ class SessionsController < ApplicationController
     @user = User.new
   end
 
+  def create
+    input_username = params[:user][:username]
+    if User.exists? (username: input_username)
+      @user = User.find_by(username: input_username)
+      if @user.password === params[:user][:password]
+        puts "YOU'RE SIGNED IN--HOORAY!"
+        session[:user_id] = @user.id
+        redirect_to root_path
+      else
+        puts "WRONG PASSWORD, DUMMY! "
+        redirect_to new_session_path
+      end
+    else
+      puts "THAT USER DOESn'TOEE IXISTETH, DUMMY!"
+      redirect_to new_session_path
+    end
+  end
+
+  def destroy
+    reset_session
+    puts "YOUR'E SIGNED OUT!"
+    redirect_to :root
+  end
 end
