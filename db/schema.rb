6145bb750488c873b46259ee881c62d5992d9b62
Nick Oki <nick.oki@live.com>
2016-10-11 15:06:36 -0400

    Start

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -31,9 +30,8 @@ ActiveRecord::Schema.define(version: 20160314195903) do
     t.string  "preview_url"
     t.integer "artist_id"
     t.integer "user_id"
+    t.index ["artist_id"], name: "index_songs_on_artist_id", using: :btree
   end
-
-  add_index "songs", ["artist_id"], name: "index_songs_on_artist_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string "username"
